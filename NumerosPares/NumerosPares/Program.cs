7a599615a05535715c7c0d59fd2de1206c00bb4e
Juliano Botelho <julianobtech@gmail.com>
2025-01-11 22:09:18 -0300

    Numeros pares

---
+++ NumerosPares/NumerosPares/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class NumerosPares {
+    static void Main(string[] args) {
+
+        Console.WriteLine("Números pares entre 1 a 100:");
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) // Verifica se o número é par
+            {
+                Console.WriteLine(i);
+            }
+        }
+        Console.WriteLine("Fim do programa!");
+    }
+}
