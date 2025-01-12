39bdb892d175f92dd704424ddecd998adaeddf04
Juliano Botelho <julianobtech@gmail.com>
2025-01-12 17:23:23 -0300

    Tabuada

---
+++ Tabuada/Tabuada/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Tabuada {
+    static void Main() {
+
+        Console.Write("Digite um número inteiro positivo: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero <= 0) {
+            Console.WriteLine("Erro: O número deve ser positivo.");
+        }
+        else {
+            for (int i = 0; i <= 10; i++) {
+                int resultado = numero * i;
+                Console.WriteLine($"{numero} x {i} = {resultado}");
+            }
+            Console.WriteLine("Fim do programa.");
+        }
+    }
+}
