be042991a32b5f8200eb3c09f22480a5f728abe2
Juliano Botelho <julianobtech@gmail.com>
2025-01-13 21:31:24 -0300

    Contagem vogais

---
+++ ContagemVogais/ContagemVogais/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class ContagemVogais {
+    static void Main(string[] args) {
+
+        Console.Write("Digite uma letra ou frase: ");
+        string entrada = Console.ReadLine();
+
+        int contagem = 0;
+        string vogais = "aeiouAEIOU";
+        
+        // Para cada caractere da entrada vai verificar se é vogal.
+        foreach (char caractere in entrada) { 
+            if (vogais.Contains(caractere)) {
+                contagem++;
+                Console.Write($"{caractere}, ");    
+            }
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"A entrada contém {contagem} vogais");
+    }
+}
